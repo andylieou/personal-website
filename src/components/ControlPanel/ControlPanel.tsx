a0ca3c2206fda664c3a7e09fe0ab293dcd24d981
andylieou <133063455+andylieou@users.noreply.github.com>
2025-05-19 21:42:54 -0500

    feat: created control panel

---
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import ControlPanelButton from "./ControlPanelButton";
+import HomePage from "../HomePage/HomePage";
+
+const Buttons = () => {
+  return (
+    <div className="panel">
+      <ControlPanelButton title="Home" page="/" />
+      <ControlPanelButton title="About Me" page="/about-me" />
+      <ControlPanelButton title="Projects" page="/projects" />
+      <ControlPanelButton title="Interests" page="/interests" />
+      <ControlPanelButton title="Connect" page="/connect" />
+    </div>
+  );
+};
+
+const Panel = () => {
+  const location = useLocation();
+  const onHomePage = location.pathname === "/";
+
+  return (
+    <>
+      {!onHomePage && <Buttons />}
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+      {onHomePage && <Buttons />}
+      
+    </>
+  );
+};
+
+const ControlPanel = () => {
+  return (
+    <>
+      <Router>
+        <Panel />
+      </Router>
+    </>
+  );
+};
+
+export default ControlPanel;
---
+++ src/components/ControlPanel/ControlPanelButton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Control.css"
+
+interface ControlPanelButtonProps {
+  title: string;
+  page: string;
+}
+
+const ControlPanelButton = ({ title, page }: ControlPanelButtonProps) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(page);
+  };
+
+  return (
+    <>
+      <button className="control-button" onClick = {handleClick}>{title}</button>
+    </>
+  );
+};
+
+export default ControlPanelButton;
