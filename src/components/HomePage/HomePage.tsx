63b08efeb7026b4226116878219981affb17884f
andylieou <133063455+andylieou@users.noreply.github.com>
2025-05-19 21:46:33 -0500

    fix: removed unnecessary divs

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -6,9 +6,7 @@ const HomePage = () => {
   return (
     <>
       <div className="container">
-        <div style =  {{height: "100px"}}/>
         <TypingAnimation write="Hi! I'm Andy Lieou" />
-        <div style =  {{height: "20px"}}/>
       </div>
     </>
   );
