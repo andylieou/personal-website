28c3c719e872d20efb35bdba15090bf750cd4d87
andylieou <133063455+andylieou@users.noreply.github.com>
2025-05-20 16:42:19 -0500

    feat: added construction milo

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -5,6 +5,10 @@ const NotFoundPage = () => {
         <div style={{ textAlign: "center", paddingTop: "20vh" }}>
             <h1>404</h1>
             <p>Oops! That page doesn’t exist.</p>
+            <img
+                src="/construction-milo.png"
+                style={{ width: "250px" }}
+            ></img>
         </div>
     );
 };
