872974e0b0f49e5db7fbf2e7b73e4621cfa72876
andylieou <133063455+andylieou@users.noreply.github.com>
2025-05-21 17:18:21 -0500

    feat: created focus page

--- src/components/Pages/Focus/Focus.tsx
+++ src/components/Pages/Focus/Focus.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import "./Focus.css";
 import { Timer } from "../../Shared";
 
 const Focus = () => {
     return (
         <>
-            <Timer />
+            <div className="focus-container">
+                <Timer />
+                <div>
+                    <iframe
+                        className="focus-video"
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/jfKfPfyJRdk?si=wRKT1jeCpuZW6cJV"
+                        title="Lofi"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                </div>
+            </div>
         </>
     );
 };
