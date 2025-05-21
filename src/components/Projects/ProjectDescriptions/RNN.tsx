ca3dc975a9176f85841639f9318c76e9c2c70633
andylieou <133063455+andylieou@users.noreply.github.com>
2025-05-20 22:19:22 -0500

    chore: ran prettier

--- src/components/Projects/ProjectDescriptions/RNN.tsx
+++ src/components/Projects/ProjectDescriptions/RNN.tsx
@@ -7,10 +7,10 @@ const RNN = () => {
                 Built RNNs for sequence modeling tasks, including a copy
                 task where kernel visualizations revealed a “store and
                 dump” memory strategy. Implemented another RNN using
-                backpropagation through time for next-word
-                prediction, analyzing hidden state dynamics and visualizing
-                cell states to interpret how the model retained and
-                processed contextual information across sequences.
+                backpropagation through time for next-word prediction,
+                analyzing hidden state dynamics and visualizing cell states
+                to interpret how the model retained and processed
+                contextual information across sequences.
             </p>
             <div style={{ height: "10px" }} />
             <p className="project-body">PyTorch, Jupyter Notebook</p>
