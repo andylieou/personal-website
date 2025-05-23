0c238c303cb3eb8e102f84bdbb1fac79c1476b65
andylieou <133063455+andylieou@users.noreply.github.com>
2025-05-23 13:56:45 -0500

    chore: ran prettier

--- src/components/Pages/Connect/Connect.tsx
+++ src/components/Pages/Connect/Connect.tsx
@@ -5,32 +5,30 @@ const Connect = () => {
     return (
         <>
             <div className="connect-page">
-                
-                    <ConnectIcon
-                        image="/linkedin.png"
-                        link="https://www.linkedin.com/in/andy-lieou-165a85268"
-                    />
-                    <ConnectIcon
-                        image="/github.png"
-                        link="https://github.com/andylieou"
-                    />
-                    <ConnectIcon
-                        image="/gitlab.svg"
-                        link="https://gitlab.com/andylieou"
-                    />
-                    <ConnectIcon
-                        image="/twitter.png"
-                        link="https://x.com/andylieou"
-                    />
-                    <ConnectIcon
-                        image="/instagram.webp"
-                        link="https://www.instagram.com/andylieou/"
-                    />
-                
+                <ConnectIcon
+                    image="/linkedin.png"
+                    link="https://www.linkedin.com/in/andy-lieou-165a85268"
+                />
+                <ConnectIcon
+                    image="/github.png"
+                    link="https://github.com/andylieou"
+                />
+                <ConnectIcon
+                    image="/gitlab.svg"
+                    link="https://gitlab.com/andylieou"
+                />
+                <ConnectIcon
+                    image="/twitter.png"
+                    link="https://x.com/andylieou"
+                />
+                <ConnectIcon
+                    image="/instagram.webp"
+                    link="https://www.instagram.com/andylieou/"
+                />
             </div>
             <div className="connect-text-container">
-                    <p className="connect-text">andylieou@gmail.com</p>
-                </div>
+                <p className="connect-text">andylieou@gmail.com</p>
+            </div>
         </>
     );
 };
